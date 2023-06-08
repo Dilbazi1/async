b50d11448435686625cb63e2ccd65b1db4c31e42
dilbazi <you@example.com>
2023-06-08 23:03:37 +0400

    homework1 f

--- task6.py
+++ task6.py
@@ -1,24 +1,4 @@
-# """Модуль file_system"""
-#
-# # получаем кодировку для файла, с которым работаем
-# F_N = open('test.txt', 'w', encoding='utf-8')
-# F_N.write('test test test')
-# F_N.close()
-# print(type(F_N))
-#
-# # явное указание кодировки при работе с файлом
-# with open('test.txt', encoding='utf-8') as f_n:
-#     for el_str in f_n:
-#         print(el_str, end='')
-#
-#
-# # УЗНАТЬ КОДИРОВКУ ФАЙЛА
-# # перекодировать, rb
-# # chardet
-# # dict -> json -> byte
-# Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое
-# программирование», «сокет», «декоратор». Проверить кодировку файла по умолчанию.
-# Принудительно открыть файл в формате Unicode и вывести его содержимое.
+
 import chardet as chardet
 
 strings = [
