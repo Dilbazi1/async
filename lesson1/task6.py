bd57735594d40fcb3e9f697e89e7943769559e4e
dilbazi <you@example.com>
2023-06-10 20:19:21 +0400

    homework2

--- lesson1/task6.py
+++ lesson1/task6.py
@@ -6,10 +6,10 @@ strings = [
     'сокет',
     'декоратор',
 ]
-with open('test_file.txt','w' ) as f_n:
+with open('test_file.txt', 'w') as f_n:
      for el_str in strings:
          f_n.write(el_str + '\n')
-with open('test_file.txt','rb' ) as f_n:
+with open('test_file.txt', 'rb') as f_n:
     print(f'Кодировка: {chardet.detect(f_n.read())["encoding"]}')
 with open('test_file.txt', 'r', encoding='raw_unicode_escape') as f_n:
     print(f_n.read())
