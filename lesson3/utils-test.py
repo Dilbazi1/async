6bfd49c3fa8f2476855d84b61495f591dab5c69a
dilbazi <you@example.com>
2023-06-19 20:47:01 +0400

    homework4 finish

--- lesson3/utils-test.py
+++ lesson3/utils-test.py
@@ -12,13 +12,36 @@ from common.variables import ERROR, ENCODING, MAX_PACKAGE_LENGTH
 import unittest
 
 from common.utils import get_message, send_message
+class TestSocket:
+    def __init__(self, message):
+        self.encoded_message = json.dumps(message).encode(ENCODING)
+
+    def recv(self, package_length=MAX_PACKAGE_LENGTH):
+        return self.encoded_message
+
+    def send(self, message):
+        self.send = json.loads(message.decode(ENCODING))
+
+class UtilsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.time = 12
+        self.message = {"action": "msg", "time": self.time}
+
+        self.test_sock = TestSocket(self.message)
 
 
-class TestUtils(unittest.TestCase):
-    def test_process_ans_error(self):
-        sock={'action': 'presence', 'time': 1687109514.307525, 'user': {'account_name': 'Guest'}}
 
-        self.assertRaises(ValueError, get_message(socket),)
+
+
+
+    def test_get_message(self):
+        test_sock = TestSocket(self.message)
+        message = get_message(test_sock)
+        self.assertEqual(message, self.message)
+
+    def test_send_message(self):
+        send_message(self.test_sock, self.message)
+        self.assertEqual(self.test_sock.send, self.message)
 
 
 if __name__ == "__main__":
