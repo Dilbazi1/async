11d497b0eb4555532c72efd2d3386e59a4a533af
dilbazi <you@example.com>
2023-06-30 20:37:28 +0400

    lesson 7

--- lesson3/common/utils.py
+++ lesson3/common/utils.py
@@ -1,23 +1,42 @@
+"""Утилиты"""
+
 import json
+import sys
+from common.variables import MAX_PACKAGE_LENGTH, ENCODING
+from errors import IncorrectDataRecivedError, NonDictInputError
+from decorator import log
+sys.path.append('../')
 
-from variables import MAX_PACKAGE_LENGTH, ENCODING
 
-
-
-def get_message(sock):
-    print(sock)
-    encoded_response = sock.recv(MAX_PACKAGE_LENGTH)
-    print(encoded_response)
+@log
+def get_message(client):
+    """
+    Утилита приёма и декодирования сообщения принимает байты выдаёт словарь,
+    если приняточто-то другое отдаёт ошибку значения
+    :param client:
+    :return:
+    """
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
         json_response = encoded_response.decode(ENCODING)
         response = json.loads(json_response)
         if isinstance(response, dict):
             return response
-        raise ValueError
-    raise ValueError
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
+@log
 def send_message(sock, message):
+    """
+    Утилита кодирования и отправки сообщения
+    принимает словарь и отправляет его
+    :param sock:
+    :param message:
+    :return:
+    """
+    if not isinstance(message, dict):
+        raise NonDictInputError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
