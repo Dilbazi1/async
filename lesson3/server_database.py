48d20d24da7c971d16042fd6696efd3743c1b7d2
dilbazi <you@example.com>
2023-07-14 14:23:46 +0400

    orm f

--- lesson3/server_database.py
+++ lesson3/server_database.py
@@ -9,7 +9,7 @@ class ServerStorage:
     # Класс - отображение таблицы всех пользователей
     # Экземпляр этого класса = запись в таблице AllUsers
     class AllUsers:
-        def __int__(self, username):
+        def __init__(self, username):
             self.name = username
             self.last_login = datetime.datetime.now()
             self.id = None
@@ -17,7 +17,7 @@ class ServerStorage:
     # Класс - отображение таблицы активных пользователей:
     # Экземпляр этого класса = запись в таблице ActiveUsers
     class ActiveUsers:
-        def __int__(self, user_id, ip_address, port, login_time):
+        def __init__(self, user_id, ip_address, port, login_time):
             self.user = user_id
             self.ip_address = ip_address
             self.port = port
@@ -49,7 +49,9 @@ class ServerStorage:
         users_table = Table('Users', self.metadata,
                             Column('id', Integer, primary_key=True),
                             Column('name', String, unique=True),
-                            Column('last_login', DateTime))
+                            Column('last_login', DateTime)
+                            )
+
         # Создаём таблицу активных пользователей
         active_users_table = Table('Active_users', self.metadata,
                                    Column('id', Integer, primary_key=True),
@@ -57,15 +59,17 @@ class ServerStorage:
                                    Column('ip_address', String),
                                    Column('port', Integer),
                                    Column('login_time', DateTime)
-
                                    )
+
         # Создаём таблицу истории входов
         user_login_history = Table('Login_history', self.metadata,
                                    Column('id', Integer, primary_key=True),
                                    Column('name', ForeignKey('Users.id')),
                                    Column('date_time', DateTime),
                                    Column('ip', String),
-                                   Column('port', String))
+                                   Column('port', String)
+                                   )
+
         # Создаём таблицы
         self.metadata.create_all(self.database_engine)
 
@@ -89,7 +93,7 @@ class ServerStorage:
         print(username, ip_address, port)
         # Запрос в таблицу пользователей на наличие там пользователя с таким именем
         rez = self.session.query(self.AllUsers).filter_by(name=username)
-        # print(type(rez))
+        #print(type(rez))
         # Если имя пользователя уже присутствует в таблице, обновляем время последнего входа
         if rez.count():
             user = rez.first()
@@ -101,6 +105,7 @@ class ServerStorage:
             self.session.add(user)
             # Комит здесь нужен, чтобы присвоился ID
             self.session.commit()
+
         # Теперь можно создать запись в таблицу активных пользователей о факте входа.
         # Создаем экземпляр класса self.ActiveUsers, через который передаем данные в таблицу
         new_active_user = self.ActiveUsers(user.id, ip_address, port, datetime.datetime.now())
@@ -131,7 +136,7 @@ class ServerStorage:
     def users_list(self):
         query = self.session.query(
             self.AllUsers.name,
-            self.AllUsers.last_login
+            self.AllUsers.last_login,
         )
         # Возвращаем список кортежей
         return query.all()
@@ -143,17 +148,28 @@ class ServerStorage:
             self.AllUsers.name,
             self.ActiveUsers.ip_address,
             self.ActiveUsers.port,
-            self.ActiveUsers.login_time).join(self.AllUsers)
+            self.ActiveUsers.login_time
+            ).join(self.AllUsers)
         # Возвращаем список кортежей
         return query.all()
 
+    # Функция возвращающая историю входов по пользователю или всем пользователям
     def login_history(self, username=None):
         # Запрашиваем историю входа
         query = self.session.query(self.AllUsers.name,
                                    self.LoginHistory.date_time,
                                    self.LoginHistory.ip,
-                                   self.LoginHistory.port).join(self.AllUsers)
+                                   self.LoginHistory.port
+                                   ).join(self.AllUsers)
         # Если было указано имя пользователя, то фильтруем по нему
         if username:
             query = query.filter(self.AllUsers.name == username)
         return query.all()
+
+# Отладка
+if __name__ == '__main__':
+    db=ServerStorage()
+    db.user_login('test1','192.168.1.1',9999)
+    db.user_login('test2','192.168.1.2',9699)
+
+    print(db.active_users_list())
